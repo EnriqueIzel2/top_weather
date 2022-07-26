a0a421588e82a66a887a3b7ab4379a758021b85b
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-25 21:49:47 -0400

    feat: change the types

--- lib/models.dart
+++ lib/models.dart
@@ -1,25 +1,53 @@
 class WeatherResponse {
-  final String cityName;
-  final TemperatureInfo tempInfo;
+  final String? cityName;
+  final TemperatureInfo? tempInfo;
+  final WeatherInfo? weatherInfo;
 
-  WeatherResponse({required this.cityName, required this.tempInfo});
+  String get iconUrl {
+    return 'https://openweathermap.org/img/wn/${weatherInfo?.icon}@2x.png';
+  }
+
+  WeatherResponse({
+    this.cityName,
+    this.tempInfo,
+    this.weatherInfo,
+  });
 
   factory WeatherResponse.fromJson(Map<String, dynamic> json) {
     final cityName = json['name'];
+
     final tempInfoJson = json['main'];
     final tempInfo = TemperatureInfo.fromJson(tempInfoJson);
 
-    return WeatherResponse(cityName: cityName, tempInfo: tempInfo);
+    final weatherInfoJson = json['weather'][0];
+    final weatherInfo = WeatherInfo.fromJson(weatherInfoJson);
+
+    return WeatherResponse(
+        cityName: cityName, tempInfo: tempInfo, weatherInfo: weatherInfo);
   }
 }
 
 class TemperatureInfo {
-  final double temperature;
+  final double? temperature;
 
-  TemperatureInfo({required this.temperature});
+  TemperatureInfo({this.temperature});
 
   factory TemperatureInfo.fromJson(Map<String, dynamic> json) {
     final temperature = json["temp"];
     return TemperatureInfo(temperature: temperature);
+  }
+}
+
+class WeatherInfo {
+  final String? description;
+  final String? icon;
+
+  WeatherInfo(this.description, this.icon);
+
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+    final description = json['description'];
+    final icon = json['icon'];
+
+    return WeatherInfo(description, icon);
   }
 }
