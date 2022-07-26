2dbe3f4075b3fd5f7b28b8f2c9bca9efd48db10b
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-25 21:49:29 -0400

    feat: add new parameter

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -3,13 +3,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:top_weather/models.dart';
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
 class DataService {
   Future<WeatherResponse> getWeather(String city) async {
     final queryParameters = {
       'q': city,
-      "appid": '29c1b0b2b0c8a3c5dd9651b166f9e8b0'
+      "appid": '29c1b0b2b0c8a3c5dd9651b166f9e8b0',
+      'units': 'imperial'
     };
     final uri = Uri.https(
         'api.openweathermap.org', '/data/2.5/weather', queryParameters);
