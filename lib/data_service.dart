b3538f984756ff321884a1fc7d4654c7efa51f76
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-25 20:39:10 -0400

    feat: change the type of getWeather

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:top_weather/models.dart';
 
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 class DataService {
-  void getWeather(String city) async {
+  Future<WeatherResponse> getWeather(String city) async {
     final queryParameters = {
       'q': city,
       "appid": '29c1b0b2b0c8a3c5dd9651b166f9e8b0'
@@ -12,7 +15,8 @@ class DataService {
         'api.openweathermap.org', '/data/2.5/weather', queryParameters);
 
     final response = await http.get(uri);
+    final json = jsonDecode(response.body);
 
-    print(response.body);
+    return WeatherResponse.fromJson(json);
   }
 }
